08467c36c0dc732719e38af894219d7d33d905d2
Alessandro Pischedda <alessandro.pischedda@gmail.com>
2015-03-19 21:52:37 +0100

    attack.py - now respect PEP8 .. in some way

--- interpreter/attack.py
+++ interpreter/attack.py
@@ -1,99 +1,101 @@
 """
-
-This file defines classes for the three kinds of attacks ammitted by the attack specification language.
-Every class includes a method that returns an XML block related to the attack execution.
-
-Authors:
-Alessandro Pischedda	alessandro.pischedda@gmail.com
-Marco Tiloca		marco.tiloca84@gmail.com
-
+This file defines classes for the three kinds of attacks ammitted by the 
+attack specification language. Every class includes a method that returns 
+ an XML block related to the attack execution.
 """
 
+__author__ = "Alessandro Pischedda, Marco Tiloca"
+__email__ = "alessandro.pischedda@gmail.com, marco.tiloca84@gmail.com"
+
 class PhysicalAttack:
-"""Return a string in XML format which contains all the Physical attacks"""
-	time = "" # attack occurrence time
-	nodes = "" # list of involved nodes
-	stmts = [] # list of actions
+    """Return a string in XML format which contains all the Physical 
+    attacks"""
 
-	# t - time , n - nodes, s - statements
-	def __init__(self, t, n, s):
-		self.time = t
-		self.nodes = n
-		self.stmts = list(s)
+    time = "" # attack occurrence time
+    nodes = "" # list of involved nodes
+    stmts = [] # list of actions
 
-	def __str__(self):
-		xml = "\n<Attack>\n\t<start_time>"+self.time+"</start_time>"
+    # t - time , n - nodes, s - statements
+    def __init__(self, t, n, s):
+        self.time = t
+        self.nodes = n
+        self.stmts = list(s)
 
-		xml = xml+"\n\t<node>"+self.nodes+"</node>"
+    def __str__(self):
+        xml = '''\n<Attack>\n\t<start_time>%s</start_time>
+                \n\t<node>%s</node>''' %(self.time, self.nodes)
 
-		for stmt in self.stmts:
-			xml = xml +"\n"+str(stmt)
+        for stmt in self.stmts:
+            xml = xml +'\n%s' %(str(stmt))
 
-		xml = xml+"\n</Attack>\n"
+        xml = xml+'\n</Attack>\n'
 
-		return xml
+        return xml
 
 
 class ConditionalAttack:
-"""Return a string in XML format which contains all the Conditional attacks"""
-	time = ""  # attack first occurrence time
-	nodes = "" # list of involved nodes
-	stmts = [] # list of actions
-	var_dict = {} # dictionary of variables <name, XML string>
-	packetFilter = "" # packet filter
+    """Return a string in XML format which contains all the Conditional
+    attacks"""
 
-	# t - time , n - nodes, v - variables, s - statements, f - packet filter
-	def __init__(self, t, n, v, s, f):
-		self.time = t
-		self.nodes = n
-		self.stmts = list(s)
-		self.var_dict = dict(v)
-		self.stmts = list(s)
-		self.packetFilter = f
+    time = ""  # attack first occurrence time
+    nodes = "" # list of involved nodes
+    stmts = [] # list of actions
+    var_dict = {} # dictionary of variables <name, XML string>
+    packetFilter = "" # packet filter
 
-	def __str__(self):
-		xml = "\n<Attack>\n\t<start_time>"+self.time+"</start_time>"
-		
-		xml = xml+"\n\t<node>"+self.nodes+"</node>"
+    # t - time , n - nodes, v - variables, s - statements, f - packet filter
+    def __init__(self, t, n, v, s, f):
+        self.time = t
+        self.nodes = n
+        self.stmts = list(s)
+        self.var_dict = dict(v)
+        self.stmts = list(s)
+        self.packetFilter = f
 
-		for key in self.var_dict.keys():
-			xml = xml + "\n\t<var><name>"+str(key)+"</name>"+str(self.var_dict[key])+"</var>"
+    def __str__(self):
+        xml = '''\n<Attack>\n\t<start_time>%s</start_time>
+		         \n\t<node>%s</node>''' %(self.time, self.nodes)
 
-		xml = xml + "\n\t<filter>" + self.packetFilter + "</filter>\n"
+        for key in self.var_dict.keys():
+            xml = xml + '\n\t<var><name>%s</name>%s</var>' %(str(key), 
+                            str(self.var_dict[key]))
+
+        xml = xml + '\n\t<filter>%s</filter>\n' %(self.packetFilter)
 			
-		for stmt in self.stmts:
-			xml = xml + "\n"+str(stmt)
+        for stmt in self.stmts:
+            xml = xml + '\n%s' %(str(stmt))
 
-		xml = xml+"\n</Attack>\n"
-
-		return xml
+        xml = xml+'\n</Attack>\n'
+        return xml
 
 
 class UnconditionalAttack:
-"""Return a string in XML format which contains all the Unconditional attacks"""
-	time = "" # attack first occurrence time
-	stmts = [] # list of actions
-	var_dict = {} # dictionary of variables <name, XML string>
-	frequency = "" # attack repetition frequency
+    """Return a string in XML format which contains all the 
+    Unconditional attacks"""
 
-	# t - time , f - frequency , v - variables, s - statements
-	def __init__(self, t, f, v, s):
-		self.time = t
-		self.stmts = list(s)
-		self.var_dict = dict(v)
-		self.frequency = f
+    time = "" # attack first occurrence time
+    stmts = [] # list of actions
+    var_dict = {} # dictionary of variables <name, XML string>
+    frequency = "" # attack repetition frequency
 
-	def __str__(self):
-		xml = "\n<Attack>\n\t<start_time>"+self.time+"</start_time>"
+    # t - time , f - frequency , v - variables, s - statements
+    def __init__(self, t, f, v, s):
+        self.time = t
+        self.stmts = list(s)
+        self.var_dict = dict(v)
+        self.frequency = f
 
-		xml = xml + "\n\t<frequency>"+self.frequency+"</frequency>"
+    def __str__(self):
+        xml = '''\n<Attack>\n\t<start_time>%s</start_time>
+                \n\t<frequency>%s</frequency>''' %(self.time, self.frequency)
 
-		for key in self.var_dict.keys():
-			xml = xml+"\n\t<var><name>"+str(key)+"</name>"+str(self.var_dict[key])+"</var>"
+        for key in self.var_dict.keys():
+            xml = xml+'\n\t<var><name>%s</name>%s</var>' % (str(key), 
+                        str(self.var_dict[key]))
 
-		for stmt in self.stmts:
-			xml = xml +"\n"+str(stmt)
+        for stmt in self.stmts:
+            xml = xml +'\n%s' %(str(stmt))
 
-		xml = xml+"\n</Attack>\n"
+        xml = xml+'\n</Attack>\n'
 
-		return xml
+        return xml
